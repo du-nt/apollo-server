55f0da0cc581e9586f8bbe2e1ae4b7726a82550e
yaokaoya <mahoagray@gmail.com>
2021-12-04 20:35:37 +0900

    updated

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -12,7 +12,10 @@ export default {
     renewToken: async (_, { token }, { controllers }) => await controllers.user.renewToken(token)
   },
   Mutation: {
-    register: async (_, { registerInput }, { controllers }) => await controllers.user.register(registerInput),
+    register: async (_, { registerInput }, { controllers }) => {
+      console.log(registerInput)
+      return await controllers.user.register(registerInput)
+    },
     login: async (_, args, { controllers }) => await controllers.user.login(args),
     socialLogin: async (_, { idToken }, { controllers }) => await controllers.user.socialLogin(idToken),
     logout: async (_, args, { controllers }) => await controllers.user.logout()
